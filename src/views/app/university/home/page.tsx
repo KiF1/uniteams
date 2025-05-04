be84127295781e4b8a9d79f6d73c39d3821da821
Giovanna Pergentino <avanade.gpcsilva@voegol.com.br>
2025-05-04 00:24:38 -0300

    Correção de conflito

--- src/views/app/university/home/page.tsx
+++ src/views/app/university/home/page.tsx
@@ -5,10 +5,7 @@ import { Metrics } from "./components/metrics"
 import { DropdownFilter } from "./components/dropdown-filter"
 import { useSearchParams } from "react-router-dom"
 
-<<<<<<< HEAD
 export const HomeUniversity = () => {
-=======
-export const Home = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   console.log(searchParams)
 
@@ -24,7 +21,6 @@ export const Home = () => {
     });
   }
 
->>>>>>> caa057835fb49282c326e503aaf08b99ea648b4b
   return (
     <section className="w-full grid gap-4">
       <AppTitle title="Solicitações de Recomendação" text="Acompanhe as solicitações das equipes!" />
